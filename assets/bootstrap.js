ffddf1be694938a7eb4f6551d5330f077971a23e
MaryelN <maryel.navarromtz@gmail.com>
2024-07-09 22:08:16 +0200

    style with sass using folders with main files, forward and use

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -9,5 +9,5 @@
 //     true,
 //     /\.[jt]sx?$/
 // ));
-// // register any custom, 3rd party controllers here
-// // app.register('some_controller_name', SomeImportedController);
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
