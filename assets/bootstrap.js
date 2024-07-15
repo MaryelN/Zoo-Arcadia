9eb162cba1122edd33348e842e5645e4b38e9c0f
MaryelN <maryel.navarromtz@gmail.com>
2024-07-15 13:42:37 +0200

    Install of vichuploader and link connexion to DashboardAdmin

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,27 +1,3 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-import { startStimulusApp } from '@symfony/stimulus-bridge';
-
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/
-));
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-import { startStimulusApp } from '@symfony/stimulus-bridge';
-
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/
-));
-// import { startStimulusApp } from '@symfony/stimulus-bundle';
-
 // const app = startStimulusApp();
 // import { startStimulusApp } from '@symfony/stimulus-bridge';
 
@@ -31,5 +7,3 @@ export const app = startStimulusApp(require.context(
 //     true,
 //     /\.[jt]sx?$/
 // ));
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
