6b9e57a3f7d1349aec5a803664f7bb59aed54c33
MaryelN <maryel.navarromtz@gmail.com>
2024-07-15 17:40:31 +0200

    creation of form for submission of comments

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Entity\Traits\Timestamp;
@@ -29,6 +30,17 @@ class Comment
 
     #[ORM\Column(type: 'boolean', options: ['default' => false])]
     private ?bool $validation = false;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Email = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $rating = null;
+
+    public function __construct()
+    {
+        $this->timestamp = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -79,6 +91,30 @@ class Comment
     public function setValidation(bool $validation): static
     {
         $this->validation = $validation;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): static
+    {
+        $this->rating = $rating;
 
         return $this;
     }
