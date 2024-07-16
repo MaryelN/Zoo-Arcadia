019ded6918d2f8a8a3ee4c0e6e62d441ae68f452
MaryelN <maryel.navarromtz@gmail.com>
2024-07-16 20:26:57 +0200

    creation of twig templates for gallery of animals

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,43 +3,104 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\{Action, Actions, Crud, KeyValueStore};
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\{ChoiceField, DateTimeField, IdField, EmailField, TextField};
+use Symfony\Component\Form\Extension\Core\Type\{PasswordType, RepeatedType};
+use Symfony\Component\Form\{FormBuilderInterface, FormEvent, FormEvents};
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(
+        public UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
-    public function configureCrud(Crud $crud): Crud
+
+    public function configureActions(Actions $actions): Actions
     {
-        return $crud
-            ->setEntityLabelInPlural('utilisateurs')
-            ->setEntityLabelInSingular('Utilisateur')
-            ->setPageTitle(Crud::PAGE_INDEX, 'Liste des %entity_label_plural%');
+        return $actions
+            ->add(Crud::PAGE_EDIT, Action::INDEX)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::DETAIL)
+            ;
     }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
+        $fields = [
             IdField::new('id')
-            ->hideOnForm(),
+                ->hideOnForm(), 
             TextField::new('lastname')
-            ->setLabel('Nom'),
+                ->setLabel('Nom'),
             TextField::new('name')
-            ->setLabel('Prénom'),
-            TextField::new('email'),
-            TextField::new('password')
-            ->hideOnForm()
-            ->hideOnIndex(),
-            ArrayField::new('roles')
-            ->hideOnIndex(),
+                ->setLabel('Prénom'),
+            EmailField::new('email'),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Veterinaire' => 'ROLE_VETERINARY',
+                    'Utilisateur' => 'ROLE_USER',
+                    ])
+                ->renderExpanded()
+                ->allowMultipleChoices()
+                ->hideOnIndex(),
             DateTimeField::new('timestamp')
-            ->setFormTypeOption('disabled', true)
+                ->setFormTypeOption('disabled', true)
         ];
+
+        $password = TextField::new('password')
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => '(Repeat)'],
+                'mapped' => false,
+            ])
+            ->setRequired($pageName === Crud::PAGE_NEW)
+            ->onlyOnForms()
+            ;
+        $fields[] = $password;
+
+        return $fields;
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->userPasswordHasher->hashPassword($this->getUser(), $password);
+            $form->getData()->setPassword($hash);
+        };
     }
 }
