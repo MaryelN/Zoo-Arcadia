d9546730e73bbff833fc8e4b7aaec038178ef135
MaryelN <maryel.navarromtz@gmail.com>
2024-07-17 21:53:09 +0200

    Add of habitat&race Repositories to index route and creation of /{id} habitat route

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Habitat;
+use App\Entity\Race;
+use App\Repository\HabitatRepository;
+use App\Repository\RaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/gallery', name: 'app_gallery_')]
 class GalleryController extends AbstractController
 {
-    #[Route('/gallery', name: 'app_gallery')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(
+        HabitatRepository $habitatRepository, 
+        RaceRepository $raceRepository, 
+        Request $request
+    ): Response
     {
+        $races = $raceRepository->findAll();
+        $habitats = $habitatRepository->findAll();
+
+
         return $this->render('gallery/index.html.twig', [
             'controller_name' => 'GalleryController',
             'title' => 'Le Zoo et les animaux',
+            'races' => $races,
+            'habitats' => $habitats
+        ]);
+    }
+
+    #[Route('/{id}', name:'habitat')]
+    public function details(Habitat $habitat): Response
+    {                
+        return $this->render('gallery/habitat.html.twig', [
+            'controller_name' => 'GalleryController',
+            'title'=>'Habitat',
+            'habitat'=>$habitat,
         ]);
     }
 }
